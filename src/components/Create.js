0b0159e265a73d301effebd68c5531d243123026
Neo Boledi <neoboledi21@gmail.com>
2023-09-04 15:51:32 +0200

    create update

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaPenSquare, FaClipboard } from 'react-icons/fa'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { FaPenSquare, FaTrashAlt } from 'react-icons/fa'
 
 function Create() {
     const data = Object.keys(localStorage);
@@ -8,44 +8,85 @@ function Create() {
         localStorage.removeItem(id)
         window.location.reload()
     }
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState('');
+
+
+
+    // Handle form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // eslint-disable-next-line eqeqeq
+        if (formData != '') {
+            e.preventDefault();
+            let uniqueId = localStorage.length;
+            let formDataId = uniqueId + 1;
+            localStorage.setItem(
+                formDataId, JSON.stringify({ data: formData })
+            )
+        } else {
+            alert('Please make sure you have filled your form')
+        }
+        navigate('/create');
+    }
 
     return (
         <>
 
             <div className='outer-container'>
                 <div className='content'>
-                    <h1>Creating a Questionnaire</h1>
-                    <Link to='/addForm'><button className='btn'>Add</button></Link>
+                    <h1 className='main-title'>Creating a Questionnaire</h1>
+                    <div>
+                        <h3>Add Queries</h3>
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <input
+                                    type="text"
+                                    id="querie"
+                                    className='textbox'
+                                    name="querie"
+                                    placeholder="Please type your question here!"
+                                    onChange={e => setFormData(e.target.value)}
+                                />
+                            </div>
+
+                            <button type="submit">Add</button>
+                        </form>
+                    </div>
+
                     <h2>List of Questions</h2>
                 </div>
-            </div>
-            {localStorage.length !== 0 ? (
-                data.map((key, i) => {
-                    const querie = JSON.parse(localStorage.getItem(key));
 
-                    return (
-                        <div key={i}>
-                            <div className='container'>
-                                <div className='elements'></div>
+                {localStorage.length !== 0 ? (
+                    data.map((key, i) => {
+                        const querie = JSON.parse(localStorage.getItem(key));
 
-                                <div>
-                                    <p>{querie.data}</p>
-                                </div>
+                        return (
+                            <div key={i}>
+                                <div className='create-container'>
+                                    <div className='elements'></div>
 
-                                <div className='icons'>
-                                    <Link to={`/edit/${key}`}><FaPenSquare /></Link>
-                                    <i onClick={() => handleDelete(key)}><FaClipboard /></i>
+                                    <div>
+                                        <p>{querie.data}</p>
+                                    </div>
+
+                                    <div className='icons'>
+                                        <Link to={`/edit/${key}`}><FaPenSquare /></Link>
+                                        <i onClick={() => handleDelete(key)}><FaTrashAlt /></i>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    );
-                })
-            ) : (
-                <div>
-                    <h1>No queries is the database</h1>
-                </div>
-            )}
+                        );
+                    })
+                ) : (
+                    <div>
+                        <br></br>
+                        <p>No queries is the database</p>
+                    </div>
+                )}
 
+
+            </div>
 
         </>
     )
--- src/components/Questionnaires.js
+++ src/components/Questionnaires.js
@@ -1,9 +1,124 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import { useNavigate } from 'react-router-dom'
 
 const Questionnaires = () => {
+    const data = Object.keys(localStorage);
+
+    // const navigate = useNavigate();
+    const [querieData, setQuerieData] = useState('');
+    const [selectedValue, setSelectedValue] = useState('');
+
+
+
+    // Radibox
+    const handleRadioChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (querieData !== '' && selectedValue !== '') {
+            // Create a new object to store both data and selectedValue
+            const formData = {
+                data: querieData,
+                selectedValue: selectedValue,
+            };
+
+            // Generate a unique ID for the form data (you can use a better approach)
+            let uniqueId = localStorage.length;
+            let formDataId = uniqueId + 1;
+
+            // Save the formData as a JSON string in localStorage
+            localStorage.setItem(formDataId, JSON.stringify(formData));
+        } else {
+            alert('Please make sure you have filled your form and selected a value');
+        }
+
+        // Reset the state variables
+        setQuerieData('');
+        setSelectedValue('');
+
+        alert('Your data was saved!')
+    }
+
     return (
-        <div>Questionnaires</div>
+        <>
+
+            <div className='outer-container'>
+                <div className='content'>
+                    <h1 className='main-title'>Questionnaire</h1>
+                </div>
+
+                {localStorage.length !== 0 ? (
+                    data.map((key, i) => {
+                        const querie = JSON.parse(localStorage.getItem(key));
+
+                        return (
+
+                            <>
+                                <div key={i}>
+                                    <div className='create-container'>
+                                        <div className='elements'></div>
+
+                                        <div>
+                                            <p>{querie.data}</p>
+
+                                        </div>
+
+                                        <div className='radio'>
+
+                                            <label>
+                                                <input
+                                                    type="radio"
+                                                    value="agree"
+                                                    checked={selectedValue === 'agree'}
+                                                    onChange={handleRadioChange}
+                                                />
+                                                Agree
+                                            </label>
+                                            <label>
+                                                <input
+                                                    type="radio"
+                                                    value="neutral"
+                                                    checked={selectedValue === 'neutral'}
+                                                    onChange={handleRadioChange}
+                                                />
+                                                Neutral
+                                            </label>
+
+                                            <label>
+                                                <input
+                                                    type="radio"
+                                                    value="disagree"
+                                                    checked={selectedValue === 'disagree'}
+                                                    onChange={handleRadioChange}
+                                                />
+                                                Disagree
+                                            </label>
+
+                                        </div>
+                                    </div>
+                                </div >
+                            </>
+
+                        );
+
+                    })
+                ) : (
+                    <div>
+                        <br></br>
+                        <p>No queries is the database</p>
+                    </div>
+
+                )}
+                <button type="submit" onClick={handleSubmit}>Submit</button>
+
+            </div >
+
+        </>
     )
+    // <button type="submit">Submit</button>
 }
 
 export default Questionnaires
